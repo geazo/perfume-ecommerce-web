03f0d4752e94996e8c52e9f450174f66c45b0b79
gentongbocor <jasonsteam9672@gmail.com>
2021-11-08 21:44:40 +0700

    css dll

---
+++ Aramyzda/index.php
@@ -0,0 +1,60 @@
+<?php require_once("./template/heading.php");
+
+?>
+<!-- code here -->
+<form action="#" method= "post">
+    <div class="header">
+        <div class="logoToko">
+            <div class="logo"></div>
+        </div>
+        <div class="NavBar">
+            <nav class="nav nav-pills flex-column flex-sm-row">
+                <a class="flex-sm-fill text-sm-center nav-link active" name="keHome" aria-current="page" href="index.php">Home</a>
+                <a class="flex-sm-fill text-sm-center nav-link" name="keKatalog" href="katalog.php">Catalogue</a>
+                <a class="flex-sm-fill text-sm-center nav-link" href="#">otw1</a>
+                <a class="flex-sm-fill text-sm-center nav-link" href="#">otw2</a>
+            </nav>
+        </div>
+    </div>
+
+    <div id="carouselExampleCaptions" class="carousel  slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./asset/formal_but_lux_amogus.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./asset/formal_but_lux_amogus.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./asset/formal_but_lux_amogus.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</form>
+
+<?php require_once("./template/footing.php")?>
---
+++ Aramyzda/katalog.php
@@ -0,0 +1,74 @@
+<?php require_once("./template/heading.php");
+$pilihanSort = array('Featured','Best Selling','Alhpabetical,A-Z','Alphabetical,Z-A','Price, Hight to Low','Price, Low to High','Oldest to Newest','Newest to Oldest');
+$printedTex = isset($_REQUEST[''])
+
+?>
+<!-- code here -->
+<form action="#" method= "post">
+    <div class="header">
+        <div class="logoToko">
+            <div class="logo"></div>
+        </div>
+        <div class="NavBar">
+            <nav class="nav nav-pills  flex-sm-row">
+                <a class="flex-sm-fill text-sm-center nav-link" name="keHome" aria-current="page" href="index.php" type="submit">Home</a>
+                <a class="flex-sm-fill text-sm-center nav-link active" name="keKatalog" href="katalog.php">Catalogue</a>
+                <a class="flex-sm-fill text-sm-center nav-link" href="#">otw1</a>
+                <a class="flex-sm-fill text-sm-center nav-link" href="#">otw2</a>
+            </nav>
+        </div>
+    </div>
+    <div class="kontainerUtama">
+
+    <div class="sortBy">
+      <div class="selectSort">
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Featured Items</option>
+          <?php for ($i=0; $i < 6; $i++) { 
+          ?>
+            <option value="sort<?=$i?>">sort<?= $i?></option>
+          <?php
+          }
+          ?>
+        </select>
+      </div>
+      <div class="displayTeksSort">
+        <span name="displayTeks" class="displayedTeks">Gucci Gang</span>
+      </div>
+    </div>
+    
+
+    <div class="konten">
+      <div class="navKiri">
+        <ul class="nav flex-sm-column nav-pills">
+        <?php for ($i=0; $i < 20; $i++) { 
+          ?>
+            <li class="nav-item">
+              <a class="nav-link " aria-current="page" href="#">brand <?= $i+1?></a>
+            </li>
+          <?php
+          }
+          ?>
+        </ul>
+      </div>
+          
+      <div class="katalog">
+      <?php for ($i=0; $i < 9; $i++) { 
+      ?>
+        <div class="card" style="width: 18rem;">
+          <img src="./asset/test.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h6 class="card-title">parfum mahal No.<?=$i+1?> gaes</h6>
+            <p class="card-text">Rp, 69.420.420
+             <br> buatan anak lokal terpercaya!
+            </p>
+          </div>
+        </div>
+      <?php
+      }
+      ?>
+      </div>  <!--penutup katalog -->
+     </div>       <!--penutup konten -->
+    </div>  <!--penutup kontainer utama -->
+</form>
+<?php require_once("./template/footing.php")?>
