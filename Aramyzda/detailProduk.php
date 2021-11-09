5f0282ff41e0dee02f50b44c34416bbf0cb69f55
gentongbocor <jasonsteam9672@gmail.com>
2021-11-09 12:12:31 +0700

    detail produk abal;

---
+++ Aramyzda/detailProduk.php
@@ -0,0 +1,47 @@
+<?php require_once("./template/heading.php");
+$listProduct = file_get_contents(
+    "result.json"
+);
+$listProduct = json_decode($listProduct,true);
+$product = $listProduct[0];
+$priceTeks = 'Rp '. number_format($product['price'],0,',','.');
+echo'<pre>';
+  print_r($listProduct[0]);
+echo'</pre>';
+?>
+<!-- code here -->
+<form action="#" method= "post">
+    <div class="header">
+        <div class="logoToko">
+            <div class="logo"></div>
+        </div>
+        <div class="NavBar">
+            <nav class="nav nav-pills flex-column flex-sm-row">
+                <a class="flex-sm-fill text-sm-center nav-link " name="keHome" aria-current="page" href="index.php">Home</a>
+                <a class="flex-sm-fill text-sm-center nav-link active" name="keKatalog" href="katalog.php">Catalogue</a>
+                <a class="flex-sm-fill text-sm-center nav-link" href="#">otw1</a>
+                <a class="flex-sm-fill text-sm-center nav-link" href="#">otw2</a>
+            </nav>
+        </div>
+    </div>
+
+    <div class="kontainerDetail">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-sm-12">
+                    <img class="fotoProd" src="<?=$product['image']?>" alt="" srcset="">
+                </div>
+                <div class="col-lg-6 col-sm-12 ">
+                    <div class="row namaProduk "> <?=$product['name'] ?> </div>
+                    <br>
+                    <div class="row hargaProduk"> <?=$priceTeks?> </div>
+                    <br>
+                    <div class="row descProduk"> <?=$product['description'] ?> </div>
+                </div>
+            </div>
+        </div><!--penutup detailprod -->
+    </div><!-- penutup kontainer besar -->
+    
+  
+</form>
+<?php require_once("./template/footing.php")?>
