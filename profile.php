007826057bffec3720031d1a30f1dd35feb0a94a
gentongbocor <jasonsteam9672@gmail.com>
2021-11-23 23:29:04 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -50,7 +50,7 @@
         <div class="col-7  justify-content-center border border-dark rounded-3">
           <div class="col-12 d-flex justify-content-center"><h5>Completed Transactions</h5></div>
           <div class="col-12">
-          <table class="table table-sm table-striped">
+          <table class="table table-hover table-sm table-striped">
             <thead>
               <tr>
                 <th scope="col">ID</th>
@@ -80,8 +80,54 @@
           </table>
           </div>
         </div>
-        <div class="col-12 d-flex justify-content-center">
-          probably something more .....
+        <div class="col-12 border border-2 mt-3 rounded-3 border-dark  ">
+          <div class="col-12 d-flex justify-content-center">
+            <h3>History Transactions</h3>
+          </div>
+          <div class="col-12">
+            <table class="table table-hover table-light ">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Invoice Number</th>
+                  <th scope="col">Detail Status Transaksi</th>
+                  <th scope="col">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php for ($i=0; $i < 1; $i++) { 
+                  # code...
+                ?>
+                  <tr>
+                    <th scope="row"><?=$i+1?></th>
+                    <td>IVC<?= str_pad($i+1,5,"0") ?></td>
+                    <td>Lagi di Jalan</td>
+                    <td>Pending</td>
+                  </tr>
+                  <tr>
+                    <th scope="row"><?=$i+2?></th>
+                    <td>IVC<?= str_pad($i+721,5,"0") ?></td>
+                    <td>Belum bayar</td>
+                    <td>Pending</td>
+                  </tr>
+                  <tr>
+                    <th scope="row"><?=$i+3?></th>
+                    <td>IVC<?= str_pad($i+187,5,"0") ?></td>
+                    <td>Belum dikirim seller</td>
+                    <td>Pending</td>
+                  </tr>
+                  <tr>
+                    <th scope="row"><?=$i+4?></th>
+                    <td>IVC<?= str_pad($i+123,5,"0") ?></td>
+                    <td>Payment Expired</td>
+                    <td>Canceled</td>
+                  </tr>
+                <?php
+                }
+                ?>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
