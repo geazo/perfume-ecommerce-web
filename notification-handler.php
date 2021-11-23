5ef83c0ed8261b3d4b2d2546d5f31081f68ce274
Kosmasu <93316254+Kosmasu@users.noreply.github.com>
2021-11-23 23:19:13 +0800

    Update notification-handler.php

--- notification-handler.php
+++ notification-handler.php
@@ -28,40 +28,35 @@ $order_id = $notif->order_id;
 $fraud = $notif->fraud_status;
 
 
-
-foreach ($item_details as $key => $item) {
-    $stmt = $conn -> prepare("query");
-}
-
 if ($transaction == 'capture') {
     // For credit card transaction, we need to check whether transaction is challenge by FDS or not
     if ($type == 'credit_card') {
         if ($fraud == 'challenge') {
             // TODO set payment status in merchant's database to 'Challenge by FDS'
             // TODO merchant should decide whether this transaction is authorized or not in MAP
-            echo "Transaction order_id: " . $order_id ." is challenged by FDS";
+            // echo "Transaction order_id: " . $order_id ." is challenged by FDS";
             $transaction = "challenge by FDS";
         } else {
             // TODO set payment status in merchant's database to 'Success'
-            echo "Transaction order_id: " . $order_id ." successfully captured using " . $type;
+            // echo "Transaction order_id: " . $order_id ." successfully captured using " . $type;
             $transaction = "success";
         }
     }
 } else if ($transaction == 'settlement') {
     // TODO set payment status in merchant's database to 'Settlement'
-    echo "Transaction order_id: " . $order_id ." successfully transfered using " . $type;
+    // echo "Transaction order_id: " . $order_id ." successfully transfered using " . $type;
 } else if ($transaction == 'pending') {
     // TODO set payment status in merchant's database to 'Pending'
-    echo "Waiting customer to finish transaction order_id: " . $order_id . " using " . $type;
+    // echo "Waiting customer to finish transaction order_id: " . $order_id . " using " . $type;
 } else if ($transaction == 'deny') {
     // TODO set payment status in merchant's database to 'Denied'
-    echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is denied.";
+    // echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is denied.";
 } else if ($transaction == 'expire') {
     // TODO set payment status in merchant's database to 'expire'
-    echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is expired.";
+    // echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is expired.";
 } else if ($transaction == 'cancel') {
     // TODO set payment status in merchant's database to 'Denied'
-    echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is canceled.";
+    // echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is canceled.";
 }
 try {
     $stmt = $conn -> prepare("INSERT INTO `htrans`(`id_user`, `tanggal`, `total`, `status`) VALUES (?,?,?,?)");
@@ -90,7 +85,7 @@ catch (\Exception $e) {
     echo $e->getMessage();
 }
 
-
+echo "404";
 function printExampleWarningMessage() {
     if ($_SERVER['REQUEST_METHOD'] != 'POST') {
         echo 'Notification-handler are not meant to be opened via browser / GET HTTP method. It is used to handle Midtrans HTTP POST notification / webhook.';
