c002f59b59d4f7660903d97505ed3017c3d4a91a
Kosmasu <93316254+Kosmasu@users.noreply.github.com>
2021-11-28 19:08:43 +0800

    profile dan detail profile

--- profile.php
+++ profile.php
@@ -1,140 +1,228 @@
-<?php include("./template/heading.php")?>
-<?php include("./template/header.php")?>
+<?php include("./template/heading.php") ?>
+<?php include("./template/header.php") ?>
+<?php
+$listTransaction = [];
+$iduser = $_SESSION['user-login']['id'];
+try {
+  $sql = "";
+  if (isset($_REQUEST['btn-filter'])) {
+    $sql = "SELECT * FROM htrans WHERE id_user = $iduser ";
+    if ($_REQUEST['select-status'] != "") {
+      $status = $_REQUEST['select-status'];
+      $sql = $sql . "AND status = '$status' ";
+    }
+    if ($_REQUEST['inp-tanggal']) {
+      $tanggal = $_REQUEST['inp-tanggal'];
+      $sql = $sql . "AND tanggal = '$tanggal' ";
+    }
+    if ($_REQUEST['select-order'] != "") {
+      $arrParam = explode("-", $_REQUEST['select-order']);
+      $sql = $sql . "ORDER BY ". $arrParam[0] . " " . $arrParam[1] . " ";
+    }
+  }
+  else {
+    $sql = "SELECT * FROM htrans WHERE id_user = $iduser";
+  }
+  $stmt = $conn->prepare($sql);
+  $stmt->execute();
+  $listTransaction = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+} catch (Exception $e) {
+  exit($e->getMessage());
+}
+
+$maxProductInAPage = 20;
+$maxPage = ceil(count($listTransaction) / $maxProductInAPage);
+
+$currentPage = 1;
+if (isset($_REQUEST['page'])) {
+  $currentPage = $_REQUEST['page'];
+  if ($currentPage < 1) {
+    $currentPage = 1;
+  } else if ($currentPage > $maxPage) {
+    $currentPage = $maxPage;
+  }
+}
+?>
 <!-- code here -->
 <div class="cb py-3"></div>
-<div class="container border border-black p-4" >
-    <div class="row ">
-      <div class="col-md-5 col-sm-12 align-items-center">
-        <div class="mintacenter">
-          <h3 class="align-self-center">Customer Information</h3>
+<div class="container border border-black p-4">
+  <div class="row">
+    <div class="col-12 align-items-center">
+      <div class="mintacenter">
+        <h3 class="align-self-center">Customer Information</h3>
+      </div>
+      <div class="col-12">
+        <div class="row my-2">
+          <div class="col-3">First Name </div>
+          <div class="col-1"> : </div>
+          <div class="col"><?= $_SESSION['user-login']['first_name'] ?> </div>
         </div>
-        <!-- <div class="col my-3">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgJNg5Lobsd8RuidmEPPw0bfDjSzq6LhkG1C5xYvvaWovVix_TdwCqlZ1yWYCXGU6PWEA&usqp=CAU" class="col-3 rounded-circle border border-dark ">
-        </div> -->
-        <div class="col-12">
-          <!-- <div class="row">
-            <div class="col-3">Usename </div>
-            <div class="col-1"> : </div>
-            <div class="col">  </div>
-          </div> -->
-          <div class="row my-2">
-            <div class="col-3">First Name </div>
-            <div class="col-1"> : </div>
-            <div class="col"><?=$_SESSION['user-login']['first_name'] ?> </div>
-          </div>
-          <div class="row my-2">
-            <div class="col-3">Last Name </div>
-            <div class="col-1"> : </div>
-            <div class="col"> <?=$_SESSION['user-login']['last_name']?> </div>
-          </div>
-          <div class="row my-2">
-            <div class="col-3">Email Address </div>
-            <div class="col-1"> : </div>
-            <div class="col"> <?=$_SESSION['user-login']['email'] ?> </div>
-          </div>
-          <div class="row my-2">
-            <div class="col-3">Phone Number </div>
-            <div class="col-1"> : </div>
-            <div class="col"> <?=$_SESSION['user-login']['phone'] ?> </div>
-          </div>
-          <div class="row my-2">
-            <div class="col-3">Address </div>
-            <div class="col-1"> : </div>
-            <div class="col"> <?=$_SESSION['user-login']['address'] ?> </div>
-          </div>
-          <div class="row my-2">
-            <div class="col-3">Birthday </div>
-            <div class="col-1"> : </div>
-            <div class="col"> <?= date($_SESSION['user-login']['birthdate']) ?> </div>
-          </div>
-          
+        <div class="row my-2">
+          <div class="col-3">Last Name </div>
+          <div class="col-1"> : </div>
+          <div class="col"> <?= $_SESSION['user-login']['last_name'] ?> </div>
         </div>
+        <div class="row my-2">
+          <div class="col-3">Email Address </div>
+          <div class="col-1"> : </div>
+          <div class="col"> <?= $_SESSION['user-login']['email'] ?> </div>
         </div>
-        <div class="col-md-7 col-sm-12 justify-content-center border border-dark rounded-3" >
-          <div class="col-12 d-flex justify-content-center"><h5>Completed Transactions</h5></div>
-          <div class="col-12" id="tableCompleted">
-          <table class="table table-hover table-sm table-striped">
-            <thead class="sticky-top bg-white">
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Tanggal</th>
-                <th scope="col">Total Pembelian</th>
-                <th scope="col">Details</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-              for ($i=0; $i < 10; $i++) { 
-                # code...
-              ?>
-                <tr>
-                  <th scope="row"><?= $i+1?></th>
-                  <td>tgl transaksi <?= $i+1?></td>
-                  <td>Rp, <?= number_format(($i+1)*899999, 0, ',', '.') ?> </td>
-                  <td><a href="">idk what to put here</a></td>
-                </tr>
-              <?php
-              }
-              ?>
-              
-              
-              </tr>
-            </tbody>
-          </table>
-          </div>
+        <div class="row my-2">
+          <div class="col-3">Phone Number </div>
+          <div class="col-1"> : </div>
+          <div class="col"> <?= $_SESSION['user-login']['phone'] ?> </div>
         </div>
-        <div class="col-12 border border-2 mt-3 rounded-3 border-dark  ">
-          <div class="col-12 d-flex justify-content-center">
-            <h3>History Transactions</h3>
-          </div>
-          <div class="col-12" id="tableAllTransaction">
-            <table class="table table-hover table-light ">
-              <thead class="sticky-top bg-white">
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Invoice Number</th>
-                  <th scope="col">Detail Status Transaksi</th>
-                  <th scope="col">Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php for ($i=0; $i <2; $i++) { 
-                  # code...
-                ?>
-                  <tr>
-                    <th scope="row"><?=$i+1?></th>
-                    <td>IVC<?= str_pad($i+1,5,"0",STR_PAD_LEFT) ?></td>
-                    <td>Lagi di Jalan</td>
-                    <td>Pending</td>
-                  </tr>
-                  <tr>
-                    <th scope="row"><?=$i+2?></th>
-                    <td>IVC<?= str_pad($i+721,5,"0",STR_PAD_LEFT) ?></td>
-                    <td>Belum bayar</td>
-                    <td>Pending</td>
-                  </tr>
-                  <tr>
-                    <th scope="row"><?=$i+3?></th>
-                    <td>IVC<?= str_pad($i+187,5,"0",STR_PAD_LEFT) ?></td>
-                    <td>Belum dikirim seller</td>
-                    <td>Pending</td>
-                  </tr>
-                  <tr>
-                    <th scope="row"><?=$i+4?></th>
-                    <td>IVC<?= str_pad($i+123,5,"0",STR_PAD_LEFT) ?></td>
-                    <td>Payment Expired</td>
-                    <td>Canceled</td>
-                  </tr>
-                <?php
-                }
-                ?>
-              </tbody>
-            </table>
-          </div>
+        <div class="row my-2">
+          <div class="col-3">Address </div>
+          <div class="col-1"> : </div>
+          <div class="col"> <?= $_SESSION['user-login']['address'] ?> </div>
+        </div>
+        <div class="row my-2">
+          <div class="col-3">Birthday </div>
+          <div class="col-1"> : </div>
+          <div class="col"> <?= date($_SESSION['user-login']['birthdate']) ?> </div>
         </div>
       </div>
     </div>
+
+    <h2>List Transaction</h2>
+
+    <div class="row">
+        <div class="col-2">
+          <h5 class="p-1 text-end">Filter: </h5> 
+        </div>
+        <div class="col-4">
+          <form action="" method="GET">
+            <select class="form-select mb-2" name="select-status">
+              <option value="">Status</option>
+              <option <?= ((isset($_REQUEST['select-status']) && ($_REQUEST['select-status'] == 'SUCCESS')) ? "selected" : "") ?> value="SUCCESS">Success</option>
+              <option <?= ((isset($_REQUEST['select-status']) && ($_REQUEST['select-status'] == 'FAILED')) ? "selected" : "") ?> value="FAILED">Failed</option>
+              <option <?= ((isset($_REQUEST['select-status']) && ($_REQUEST['select-status'] == 'PENDING')) ? "selected" : "") ?> value="PENDING">Pending</option>
+            </select>
+            <select class="form-select mb-2" name="select-order">
+              <option value="">Order</option>
+              <option <?= ((isset($_REQUEST['select-order']) && ($_REQUEST['select-order'] == 'total-asc')) ? "selected" : "") ?> value="total-asc">Total Asc</option>
+              <option <?= ((isset($_REQUEST['select-order']) && ($_REQUEST['select-order'] == 'total-desc')) ? "selected" : "") ?> value="total-desc">Total Desc</option>
+              <option <?= ((isset($_REQUEST['select-order']) && ($_REQUEST['select-order'] == 'tanggal-desc')) ? "selected" : "") ?> value="tanggal-desc">Newest</option>
+              <option <?= ((isset($_REQUEST['select-order']) && ($_REQUEST['select-order'] == 'tanggal-asc')) ? "selected" : "") ?> value="tanggal-asc">Oldest</option>
+            </select>
+            <input value="<?= isset($_REQUEST['inp-tanggal']) ? $_REQUEST['inp-tanggal'] : "" ?>" type="date" class="form-control mb-2" name="inp-tanggal" placeholder="Tanggal">
+            <button name="btn-filter" type="submit" class="btn btn-secondary btn-sm mb-2">Go</button>
+          </form>
+          <form action="" method="get">
+            <button name="btn-reset" type="submit" class="btn btn-secondary btn-sm mb-2">Reset</button>
+          </form>
+        </div>
+      </div>
+
+    <div class="row py-3">
+      <div class="col-3">
+      </div>
+      <div class="col-6 d-flex align-items-center justify-content-center">
+        <?php if (count($listTransaction) > 0) { ?>
+          <nav class="d-flex justify-content-center">
+            <ul class="pagination m-0">
+              <li class="page-item"><a class="page-link text-dark" href="<?= 'profile.php?page=' . $currentPage - 1 ?>">Previous</a></li>
+              <?php if ($currentPage - 2 > 1) { ?>
+                <li class="page-item"><a class="page-link text-dark" href="<?= 'profile.php' ?>">1</a></li>
+                <li class="page-item"><span class="page-link text-dark">...</span></li>
+              <?php } ?>
+              <?php for ($i = $currentPage - 2; $i <= $currentPage + 2; $i++) { ?>
+                <?php if ($i < 1 || $i > $maxPage) continue; ?>
+                <?php if ($i == $currentPage) { ?>
+                  <li class="page-item"><a class="page-link text-dark bg-secondary bg-opacity-25" href="<?= 'profile.php?page=' . $i ?>"><?= $i ?></a></li>
+                <?php } else { ?>
+                  <li class="page-item"><a class="page-link text-dark" href="<?= 'profile.php?page=' . $i ?>"><?= $i ?></a></li>
+                <?php } ?>
+              <?php } ?>
+              <?php if ($currentPage + 2 < $maxPage) { ?>
+                <li class="page-item"><span class="page-link text-dark">...</span></li>
+                <li class="page-item"><a class="page-link text-dark" href="<?= 'profile.php?page=' . $maxPage ?>"><?= $maxPage ?></a></li>
+              <?php } ?>
+              <li class="page-item"><a class="page-link text-dark" href="<?= 'profile.php?page=' . $currentPage + 1 ?>">Next</a></li>
+            </ul>
+          </nav>
+        <?php } ?>
+      </div>
+      <div class="col-3 d-flex justify-content-end align-items-center">
+        <?= (($currentPage - 1) * $maxProductInAPage) + 1 ?> -
+        <?= ($currentPage * $maxProductInAPage) + 1 > count($listTransaction) ? count($listTransaction) : ($currentPage * $maxProductInAPage) + 1 ?>
+        out of <?= count($listTransaction) ?> transaction(s)
+      </div>
+    </div>
+
+    <div class="table-responsive">
+      <table class="table table-striped table-sm align-middle">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">ID</th>
+            <th class="text-center" scope="col">Tanggal</th>
+            <th class="text-end" scope="col">Total</th>
+            <th class="text-center" scope="col">Status</th>
+            <th class="text-center" scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody id="tbody">
+          <?php for ($i = ($currentPage - 1) * $maxProductInAPage; $i < $currentPage * $maxProductInAPage; $i++) { ?>
+            <?php if ($i >= count($listTransaction)) break; ?>
+            <?php 
+              $transaction = $listTransaction[$i];
+              if ($transaction['status'] == "SUCCESS") $classColor = "bg-success";
+              else if ($transaction['status'] == "FAILED") $classColor = "bg-danger";
+              else if ($transaction['status'] == "PENDING") $classColor = "bg-warning";
+            ?>
+            <tr>
+              <td><?= ($i + 1) . "." ?></td>
+              <td><?= $transaction['id_transaksi'] ?></td>
+              <td class="text-center"><?= $transaction['tanggal'] ?></td>
+              <td class="text-end"><?= "Rp. " . getFormatHarga($transaction['total']) ?></td>
+              <td class="text-center <?=$classColor?>"><b><?= $transaction['status'] ?></b></td>
+              <td class="text-center">
+                <a href="profile_detail_transaction.php?id=<?= $transaction['id_transaksi'] ?>">
+                  <button type="button" class="btn btn-secondary">Detail</button>
+                </a>
+              </td>
+            </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    </div>
+
+    <div class="row py-3">
+      <div class="col-3">
+      </div>
+      <div class="col-6 d-flex align-items-center justify-content-center">
+        <?php if (count($listTransaction) > 0) { ?>
+          <nav class="d-flex justify-content-center">
+            <ul class="pagination m-0">
+              <li class="page-item"><a class="page-link text-dark" href="<?= 'profile.php?page=' . $currentPage - 1 ?>">Previous</a></li>
+              <?php if ($currentPage - 2 > 1) { ?>
+                <li class="page-item"><a class="page-link text-dark" href="<?= 'profile.php' ?>">1</a></li>
+                <li class="page-item"><span class="page-link text-dark">...</span></li>
+              <?php } ?>
+              <?php for ($i = $currentPage - 2; $i <= $currentPage + 2; $i++) { ?>
+                <?php if ($i < 1 || $i > $maxPage) continue; ?>
+                <?php if ($i == $currentPage) { ?>
+                  <li class="page-item"><a class="page-link text-dark bg-secondary bg-opacity-25" href="<?= 'profile.php?page=' . $i ?>"><?= $i ?></a></li>
+                <?php } else { ?>
+                  <li class="page-item"><a class="page-link text-dark" href="<?= 'profile.php?page=' . $i ?>"><?= $i ?></a></li>
+                <?php } ?>
+              <?php } ?>
+              <?php if ($currentPage + 2 < $maxPage) { ?>
+                <li class="page-item"><span class="page-link text-dark">...</span></li>
+                <li class="page-item"><a class="page-link text-dark" href="<?= 'profile.php?page=' . $maxPage ?>"><?= $maxPage ?></a></li>
+              <?php } ?>
+              <li class="page-item"><a class="page-link text-dark" href="<?= 'profile.php?page=' . $currentPage + 1 ?>">Next</a></li>
+            </ul>
+          </nav>
+        <?php } ?>
+      </div>
+      <div class="col-3 d-flex justify-content-end align-items-center">
+      </div>
+    </div>
+  </div>
 </div>
 <div class="cb py-3"></div>
-<?php require_once("./template/footer.php")?>
-<?php include("./template/footing.php")?>
+<?php require_once("./template/footer.php") ?>
+<?php include("./template/footing.php") ?>
---
+++ profile_detail_transaction.php
@@ -0,0 +1,127 @@
+<?php include("./template/heading.php") ?>
+<?php include("./template/header.php") ?>
+<?php
+$id = $_REQUEST['id'];
+$listProduct = [];
+try {
+    $stmt = $conn->prepare("SELECT * FROM dtrans d, product p WHERE p.id = d.id_product AND d.id_transaksi = ?");
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $listProduct = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+
+    $stmt = $conn->prepare("SELECT * FROM htrans h, user u WHERE h.id_user = u.id AND h.id_transaksi = ?");
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $transaction = $stmt->get_result()->fetch_assoc();
+} catch (Exception $e) {
+    exit($e->getMessage());
+}
+
+$maxProductInAPage = 10;
+$maxPage = ceil(count($listProduct) / $maxProductInAPage);
+
+$currentPage = 1;
+if (isset($_REQUEST['page'])) {
+    $currentPage = $_REQUEST['page'];
+    if ($currentPage < 1) {
+        $currentPage = 1;
+    } else if ($currentPage > $maxPage) {
+        $currentPage = $maxPage;
+    }
+}
+?>
+<div class="container-fluid my-4">
+
+    <h2>Detail Transaksi</h2>
+    <table class="fs-5">
+        <tr>
+            <td class="p-1 text-end">ID Transaksi:</td>
+            <td class="p-1"><?= $transaction['id_transaksi'] ?></td>
+        </tr>
+        <tr>
+            <td class="p-1 text-end">Nama User:</td>
+            <td class="p-1"><?= $transaction['first_name'] . " " . $transaction['last_name'] ?></td>
+        </tr>
+        <tr>
+            <td class="p-1 text-end">Tanggal & Waktu:</td>
+            <td class="p-1"><?= $transaction['tanggal'] ?></td>
+        </tr>
+        <tr>
+            <td class="p-1 text-end">Status:</td>
+            <td class="p-1"><?= $transaction['status'] ?></td>
+        </tr>
+        <tr>
+            <td class="p-1 text-end">Total:</td>
+            <td class="p-1"><?= "Rp. " . getFormatHarga($transaction['total']) ?></td>
+        </tr>
+    </table>
+
+    <div class="row py-3">
+        <div class="col-3">
+        </div>
+        <div class="col-6 d-flex align-items-center justify-content-center">
+            <?php if (count($listProduct) > 0) { ?>
+                <nav class="d-flex justify-content-center">
+                    <ul class="pagination m-0">
+                        <li class="page-item"><a class="page-link text-dark" href="<?= 'detail_transaction.php?id=' . $id . '&page=' . $currentPage - 1 ?>">Previous</a></li>
+                        <?php if ($currentPage - 2 > 1) { ?>
+                            <li class="page-item"><a class="page-link text-dark" href="<?= 'detail_transaction.php?id=' . $id ?>">1</a></li>
+                            <li class="page-item"><span class="page-link text-dark">...</span></li>
+                        <?php } ?>
+                        <?php for ($i = $currentPage - 2; $i <= $currentPage + 2; $i++) { ?>
+                            <?php if ($i < 1 || $i > $maxPage) continue; ?>
+                            <?php if ($i == $currentPage) { ?>
+                                <li class="page-item"><a class="page-link text-dark bg-secondary bg-opacity-25" href="<?= 'detail_transaction.php?id=' . $id . '&page=' . $i ?>"><?= $i ?></a></li>
+                            <?php } else { ?>
+                                <li class="page-item"><a class="page-link text-dark" href="<?= 'detail_transaction.php?id=' . $id . '&page=' . $i ?>"><?= $i ?></a></li>
+                            <?php } ?>
+                        <?php } ?>
+                        <?php if ($currentPage + 2 < $maxPage) { ?>
+                            <li class="page-item"><span class="page-link text-dark">...</span></li>
+                            <li class="page-item"><a class="page-link text-dark" href="<?= 'detail_transaction.php?id=' . $id . '&page=' . $maxPage ?>"><?= $maxPage ?></a></li>
+                        <?php } ?>
+                        <li class="page-item"><a class="page-link text-dark" href="<?= 'detail_transaction.php?id=' . $id . '&page=' . $currentPage + 1 ?>">Next</a></li>
+                    </ul>
+                </nav>
+            <?php } ?>
+        </div>
+        <div class="col-3 d-flex justify-content-end align-items-center">
+            <?= (($currentPage - 1) * $maxProductInAPage) + 1 ?> -
+            <?= ($currentPage * $maxProductInAPage) + 1 > count($listProduct) ? count($listProduct) : ($currentPage * $maxProductInAPage) + 1 ?>
+            out of <?= count($listProduct) ?> product(s)
+        </div>
+    </div>
+
+    <div class="table-responsive">
+        <table class="table table-striped table-sm align-middle">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Image</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Brand</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Quantity</th>
+                </tr>
+            </thead>
+            <tbody id="tbody">
+                <?php for ($i = ($currentPage - 1) * $maxProductInAPage; $i < $currentPage * $maxProductInAPage; $i++) { ?>
+                    <?php if ($i >= count($listProduct)) break; ?>
+                    <?php $product = $listProduct[$i] ?>
+                    <tr>
+                        <td><?= ($i + 1) . "." ?></td>
+                        <td class="text-center">
+                            <img class="hover-expand" style="width: 100px;" src="<?= $product['image_source'] ?>" alt="">
+                        </td>
+                        <td><?= $product['name'] ?></td>
+                        <td><?= $product['brand'] ?></td>
+                        <td><?= $product['type'] ?></td>
+                        <td><?= $product['quantity'] ?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+<?php require_once("./template/footer.php") ?>
+<?php include("./template/footing.php") ?>
