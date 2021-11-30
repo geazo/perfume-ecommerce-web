4eb0b686654a957a442d21907ace1badd13ec855
gentongbocor <jasonsteam9672@gmail.com>
2021-11-30 20:24:20 +0700

    index updet

--- index.php
+++ index.php
@@ -5,53 +5,92 @@
   $stmt = $conn->prepare("select * from product");
   $stmt->execute();
   $listProduct = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+  $ayebruv = $conn->prepare("select * from product order by stock desc");
+  $ayebruv->execute();
+  $listByStock = $ayebruv->get_result()->fetch_all(MYSQLI_ASSOC);
+  $podium1 = $listProduct[0];
+  $podium2 = $listByStock[0];
+  $podium3 = $listProduct[68];
+  
 ?>
 <!-- code here -->
 <!-- <form action="#" method= "post"> -->
 
 <?php include ("./template/header.php")?>
-
-    <div id="carouselExampleCaptions" class="carousel py-3 slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+<div class="row bgAboutUs text-light p-0 py-3 m-0" >
+  <div class="col-12 d-flex justify-content-center align-items-center" id='aboutus'>
+    <h2 class="" >About Us</h2>
   </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="<?= $listProduct[0]['image_source']?>" class="d-block  m-3 w-25" alt="...">
-      <div class="carousel-caption text-dark d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
+  <div class="col-12 d-flex justify-content-center " >
+    
+    <div class="col-5 text-align-end">
+        <p class="fs-4">We are a shop that wishes to bless your <br>
+          senses of smell with the Fragrance of the World. <br> 
+          Be it for a Party, a Romantic Dinner, or on the daily! <br>
+          Browse our vast collection of Fragrances 
+        </p>
     </div>
-    <div class="carousel-item">
-      <img src="./asset/formal_but_lux_amogus.png" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="./asset/formal_but_lux_amogus.png" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
+    
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-
-    <!-- <a href="index.php#idbarang"></a> auto scroll waktu linking -->
-
-  </button>
 </div>
+<div class="container ">
+
+  <div class="row h-75vh d-flex justify-content-center w-100 m-3 p-2  ">
+      <h3 class="text-center">Featured Items</h3>
+      <div id="carouselExampleCaptions" class="carousel carousel-dark py-3 slide carousel-fade vh-75" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner " style="max-height:356px;">
+      
+      <div class="carousel-item active">
+      <a href="detailProduk.php?product=<?= $podium1['id'] ?>">
+        <img src="<?= $podium1['image_source']?>" class="d-block h-100 m-3 w-25" alt="...">
+      </a>
+        <div class="carousel-caption text-dark d-none d-md-block">
+          <h5>Try our Very First Item in the Shop!</h5>
+          <p><?=$podium1['name'] ?>, one of the Originals of this Shop!</p>
+        </div>
+      </div>
+
+      <div class="carousel-item " style="max-height:356px;">
+      <a href="detailProduk.php?product=<?= $podium2['id'] ?>">
+        <img src="<?= $podium2['image_source']?>" class=" d-block h-100 m-3 w-25" alt="...">
+      </a>
+        <div class="carousel-caption text-dark d-none d-md-block">
+          <h5>The Item Everyone Wants!</h5>
+          <p>Famously on demand, <?=$podium2['name'] ?>!</p>
+        </div>
+      </div>
+
+      <div class="carousel-item  " style="max-height:356px;">
+      <a href="detailProduk.php?product=<?= $podium3['id'] ?>">
+        <img src="<?= $podium3['image_source']?>" class="d-block h-100 m-3 w-25" alt="..." >
+      </a>
+        <div class="carousel-caption text-dark d-none d-md-block">
+          <h5>Our Favourite!</h5>
+          <p>For us, <?=$podium3['name'] ?> has always been our favourite!</p>
+        </div>
+      </div>
+
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon button-dark" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon button-dark" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+  </div>
+  
+</div>
+
+
+<!-- <a href="index.php#idbarang"></a> auto scroll waktu linking -->
 <!-- </form> -->
 
 <?php require_once("./template/footer.php")?>
--- template/footer.php
+++ template/footer.php
@@ -3,16 +3,18 @@
 <div class="container">
   <footer class="py-5">
     <div class="row">
-      <div class="col-3">
+      <div class="col-md-3 col-sm-6">
         <h5>Browse</h5>
         <ul class="nav flex-column">
           <li class="nav-item mb-2 border-bottom-0"><a href="index.php" class="nav-link p-0 text-muted">Home</a></li>
           <li class="nav-item mb-2 border-bottom-0"><a href="katalog.php" class="nav-link p-0 text-muted">Catalogue</a></li>
+          <li class="nav-item mb-2 border-bottom-0"><a href="index.php#aboutus" class="nav-link p-0 text-muted">About Us</a></li>
+          
           
         </ul>
       </div>
 
-      <div class="col-3">
+      <div class="col-md-3 col-sm-6">
         <h5>Account</h5>
         <ul class="nav flex-column">
           <li class="nav-item mb-2 border-bottom-0"><a href="profile.php" class="nav-link p-0 text-muted">My Profile</a></li>
@@ -34,7 +36,7 @@
         </ul>
       </div> -->
 
-      <div class="col-4 offset-1">
+      <div class="col-md-4 col-sm-12 offset-md-1">
         <form>
           <h5>Subscribe to our newsletter</h5>
           <p>Monthly digest of whats new and on sale from us.</p>
@@ -48,7 +50,7 @@
     </div>
 
     <div class="d-flex justify-content-between py-4 my-4 border-top">
-      <p>&copy; 2021 Company, Inc. All rights reserved.</p>
+      <p>&copy; 220116925 - Kenny | 220116920 - Jason Kurniawan</p>
       <ul class="list-unstyled d-flex">
         <li class="ms-3"><a class="link-dark" href="#"><i class="fs-1 fa fa-twitter"></i></a></li>
         <li class="ms-3"><a class="link-dark" href="#"><i class="fs-1 fa fa-instagram"></i></a></li>
