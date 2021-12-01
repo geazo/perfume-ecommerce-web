dc112c3e2e2e0900c45428794c2235c5f3977906
Kosmasu <93316254+Kosmasu@users.noreply.github.com>
2021-12-01 17:06:22 +0800

    pagination bug lagi ahi

--- profile_detail_transaction.php
+++ profile_detail_transaction.php
@@ -63,7 +63,7 @@ if (isset($_REQUEST['page'])) {
             <?php if (count($listProduct) > 0) { ?>
                 <nav class="d-flex justify-content-center">
                     <ul class="pagination m-0">
-                        <li class="page-item"><a class="page-link text-dark" href="<?= 'profile_detail_transaction.php?id=' . $id . '&page=' . $currentPage - 1 ?>">Previous</a></li>
+                        <li class="page-item"><a class="page-link text-dark" href="<?= 'profile_detail_transaction.php?id=' . $id . '&page=' . ($currentPage - 1) ?>">Previous</a></li>
                         <?php if ($currentPage - 2 > 1) { ?>
                             <li class="page-item"><a class="page-link text-dark" href="<?= 'profile_detail_transaction.php?id=' . $id ?>">1</a></li>
                             <li class="page-item"><span class="page-link text-dark">...</span></li>
@@ -80,7 +80,7 @@ if (isset($_REQUEST['page'])) {
                             <li class="page-item"><span class="page-link text-dark">...</span></li>
                             <li class="page-item"><a class="page-link text-dark" href="<?= 'profile_detail_transaction.php?id=' . $id . '&page=' . $maxPage ?>"><?= $maxPage ?></a></li>
                         <?php } ?>
-                        <li class="page-item"><a class="page-link text-dark" href="<?= 'profile_detail_transaction.php?id=' . $id . '&page=' . $currentPage + 1 ?>">Next</a></li>
+                        <li class="page-item"><a class="page-link text-dark" href="<?= 'profile_detail_transaction.php?id=' . $id . '&page=' . ($currentPage + 1) ?>">Next</a></li>
                     </ul>
                 </nav>
             <?php } ?>
